54f94d7f1bce62a84848ce8eb359123e4b026b62
raju <araju2014to2018@gmail.com>
2020-01-23 12:41:28 +0530

    Initial Commit

---
+++ integration with googlemaosTests/integration_with_googlemaosTests.swift
@@ -0,0 +1,34 @@
+//
+//  integration_with_googlemaosTests.swift
+//  integration with googlemaosTests
+//
+//  Created by A RAJU on 1/23/20.
+//  Copyright © 2020 A RAJU. All rights reserved.
+//
+
+import XCTest
+@testable import integration_with_googlemaos
+
+class integration_with_googlemaosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
